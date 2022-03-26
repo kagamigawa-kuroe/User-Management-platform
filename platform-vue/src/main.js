9141a23ec84bebae155d1cac4726289d6825f838
whz <122885887@qq.com>
2022-03-26 19:17:21 +0100

    Configure vue env

---
+++ platform-vue/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Element from "element-ui"
+import "element-ui/lib/theme-chalk/index.css"
+import axios from "axios";
+
+// set global variable axios
+Vue.prototype.$axios = axios
+
+// use web UI library Element-ui
+Vue.use(Element)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ platform-vue/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+<div>home page</div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
